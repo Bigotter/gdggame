d17844b4ef161daed86aab1758c37a57b9459b12
Jorge Juan Barroso Carmona <jorge@karumi.com>
2017-08-12 19:58:26 +0200

    Refactor to extract common code

--- Assets/ApplyHappiness.cs
+++ Assets/ApplyHappiness.cs
@@ -1,25 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Net;
 using core;
 using UnityEngine;
 
-public class ApplyHappiness : MonoBehaviour {
+public class ApplyHappiness : MonoBehaviour
+{
+    ApplyIconValue _applyIconValue = new ApplyIconValue();
 
-	void Start () {
-		
-	}
-	
-	void Update ()
-	{
-		var processCard = ProcessCard.Instance();
-		var happiness = processCard.Happiness;
-		
-		var objectToCrop = transform.Find("mask");
+    void Start()
+    {
+        _applyIconValue.Init(ProcessCard.InitHappiness);
+    }
 
-		var totalWidht = objectToCrop.GetComponent<RectTransform>().sizeDelta.y;
-		var size = totalWidht * happiness / ProcessCard.MaxHappiness;
-		var moveCropper = totalWidht - size;
-		objectToCrop.GetComponent<RectTransform>().localPosition = new Vector3(0f, -moveCropper,0f);
-		
-	}
+    void Update()
+    {
+        var processCard = ProcessCard.Instance();
+        var happiness = processCard.Happiness;
+
+        _applyIconValue.Apply(gameObject, processCard.Happiness);
+    }
 }
---
+++ Assets/Scripts/ApplyIconValue.cs
@@ -0,0 +1,50 @@
+﻿using core;
+using UnityEngine;
+
+public class ApplyIconValue
+{
+    private int _previousValue;
+    private float _timeAnimColor;
+    private Color _colorHighlight = Color.white;
+
+    public void Apply(GameObject gameObjectIcon, int happiness)
+    {
+        if (_previousValue != happiness)
+        {
+            var objectToCrop = gameObjectIcon.transform.Find("mask");
+
+            var totalWidht = objectToCrop.GetComponent<RectTransform>().sizeDelta.y;
+            var size = totalWidht * happiness / ProcessCard.MaxHappiness;
+            var moveCropper = totalWidht - size;
+            objectToCrop.GetComponent<RectTransform>().localPosition = new Vector3(0f, -moveCropper, 0f);
+            _colorHighlight = _previousValue > happiness ? Color.red : Color.green;
+
+            _timeAnimColor = 0.5f;
+
+            _previousValue = happiness;
+        }
+
+        if (_colorHighlight != Color.white)
+        {
+            if (_timeAnimColor > 0)
+            {
+                var icon = gameObjectIcon.transform.Find("fill");
+                var sprite = icon.GetComponent<SpriteRenderer>();
+                sprite.color = _colorHighlight;
+                _timeAnimColor -= Time.deltaTime;
+            }
+            else
+            {
+                _colorHighlight = Color.white;
+                var icon = gameObjectIcon.transform.Find("fill");
+                var sprite = icon.GetComponent<SpriteRenderer>();
+                sprite.color = _colorHighlight;
+            }
+        }
+    }
+
+    public void Init(int initValue)
+    {
+        _previousValue = initValue;
+    }
+}
