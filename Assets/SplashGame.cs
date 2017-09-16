1d8f73498541b904ed8312b9af94788505e8742e
Jorge Juan Barroso Carmona <flipper83@gmail.com>
2017-09-16 11:05:15 +0200

    Add splash for bigotter screen and credits screen.

---
+++ Assets/SplashGame.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplashGame : MonoBehaviour {
+	private static float TIME_SPLASH = 4.0f;
+	private float _timeSplash;
+
+	public string sceneName;
+
+	void Start () {
+		_timeSplash = 0; 
+	}
+
+	void Update () {
+		_timeSplash += Time.deltaTime;
+
+		if (_timeSplash > TIME_SPLASH) {
+			SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+		}
+	}
+}
